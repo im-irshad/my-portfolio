b31ebd2e8ab6e0c305178656f520a52aaed63c30
Irshad Muhammad <imabbasi@msn.com>
2022-04-24 01:13:48 +0200

    darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 my-14 lg:px-48 sm:px-16 md:px-32">
-        <div className="lg:col-span-3 bg-white dark:bg-black rounded-2xl col-span-12 p-4 text-center">
+        <div className="lg:col-span-3 bg-white  rounded-2xl col-span-12 p-4 text-center">
           <Sidebar />
         </div>
         <div className="lg:col-span-9 bg-white rounded-2xl flex flex-col col-span-12 overflow-hidden">
