66958ba055ae0b4d2221783737fb42ba9b77bcf7
Irshad Muhammad <imabbasi@msn.com>
2022-05-12 12:41:20 +0200

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,11 @@ function index() {
     <div className="flex flex-col px-6 pt-1 flex-grow">
       <a
         href={
-          "https://api.whatsapp.com/send?text=http://staging.bygoodiebox.com/se/sv/happyinvite?utm_source=referral&utm_medium=happyinvite&invitationCode=fowlat-vy44z978"
+          "https://wa.me?text=http://staging.bygoodiebox.com/se/sv/happyinvite?utm_source=referral&utm_medium=happyinvite&invitationCode=fowlat-vy44z978"
         }
         data-action="share/whatsapp/share"
       >
-        invite
+        invite new
       </a>
       <h5 className="my-3 font-medium text-bblue">
         I am a passionate IT-professional with an experience of working with
