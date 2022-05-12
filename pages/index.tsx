0f0da840670e25b672433f828ba4ae235b2f4b23
Irshad Muhammad <imabbasi@msn.com>
2022-05-12 13:41:26 +0200

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,6 @@ import { services } from "../data";
 function index() {
   return (
     <div className="flex flex-col px-6 pt-1 flex-grow">
-      <a
-        href={
-          "whatsapp://send?text=http://localhost:3000/se/sv/happyinvite?utm_source=referral&utm_medium=happyinvite&invitationCode=fowlat-vy44z978"
-        }
-      >
-        whatsapp
-      </a>
-      <a
-        href={
-          "fb-messenger://share/?link=http://localhost:3000/se/sv/happyinvite?utm_source=referral&utm_medium=happyinvite&invitationCode=fowlat-vy44z978"
-        }
-      >
-        invite fb mess
-      </a>
-      <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.facebook.com/sharer/sharer.php?u=http%3A//localhost%3A3000/se/sv/happyinvite?utm_source=referral%26utm_medium=happyinvite%26invitationCode=fowlat-vy44z978">
-        Share on Facebook
-      </a>
-
       <h5 className="my-3 font-medium text-bblue">
         I am a passionate IT-professional with an experience of working with
         Javascript, Typescript, React, Express, Node and MongoDB. Eager to learn
