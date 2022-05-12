872f5ca551eab9a75b9821aa9a480d60546b3402
Irshad Muhammad <imabbasi@msn.com>
2022-05-12 12:32:05 +0200

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,14 @@ import { services } from "../data";
 function index() {
   return (
     <div className="flex flex-col px-6 pt-1 flex-grow">
+      <a
+        href={
+          "whatsapp://send?text=http://staging.bygoodiebox.com/se/sv/happyinvite?utm_source=referral&utm_medium=happyinvite&invitationCode=fowlat-vy44z978"
+        }
+        data-action="share/whatsapp/share"
+      >
+        invite
+      </a>
       <h5 className="my-3 font-medium text-bblue">
         I am a passionate IT-professional with an experience of working with
         Javascript, Typescript, React, Express, Node and MongoDB. Eager to learn
